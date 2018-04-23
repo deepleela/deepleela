898796d8af263c3851194da8717dd5b4bda1ea96
deepleela <leelazero@protonmail.com>
2018-04-23 22:37:06 +0800

    test colors

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <div style={{ width: '60%', height: 500, margin: 'auto' }}>
+        <div style={{ width: '45%', height: '100%', margin: 'auto' }}>
           <Board size={19} />
         </div>
       </div>
--- src/components/Intersection.tsx
+++ src/components/Intersection.tsx
@@ -65,14 +65,14 @@ export class Intersection extends React.Component<IntersectionProps, Intersectio
 
             <div style={{ float: 'left', lineHeight: 1, paddingTop: `${this.props.width}%`, width: `${this.props.width}%`, position: 'relative', textAlign: 'center' }}>
 
-                {/* Star Point */}
-                {this.props.star ? <div style={{ pointerEvents: 'none', background: gridColor, borderRadius: 9000, height: 6, width: 6, position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)' }} /> : null}
-
                 {/* Vertical Grid Line */}
                 <div style={{ pointerEvents: 'none', background: gridColor, height: this.props.lineThickness || 1, position: 'absolute', top: '50%', right: this.props.rightEdge ? '50%' : 0, left: this.props.leftEdge ? '50%' : 0, transform: 'translateY(-50%)' }} />
 
                 {/* Horizontal Grid Line */}
                 <div style={{ pointerEvents: 'none', background: gridColor, width: this.props.lineThickness || 1, position: 'absolute', left: '50%', top: this.props.topEdge ? '50%' : 0, bottom: this.props.bottomEdge ? '50%' : 0, transform: 'translateX(-50%)' }} />
+
+                {/* Star Point */}
+                {this.props.star ? <div style={{ pointerEvents: 'none', background: 'lightgrey', borderRadius: 9000, height: 6, width: 6, position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)' }} /> : null}
 
                 {/* Touch Surface */}
                 <div style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, background: 'rgba(0, 0, 0, 0)', border: this.state.hover && this.props.state === State.Empty ? '2px dashed rgba(0, 0, 0, 0.15)' : undefined, }} onMouseEnter={e => this.onMouseEnter(e)} onMouseLeave={e => this.onMouseLeave(e)} onClick={e => this.onClick(e)} />
